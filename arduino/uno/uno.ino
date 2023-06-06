75fe6ffba4e8cc7dbb96912185a84ec27d304b35
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2023-06-06 15:47:21 -0500

    Configure Digital Pins Serial

--- arduino/uno/uno.ino
+++ arduino/uno/uno.ino
@@ -7,13 +7,21 @@
 */
 
 int digitalConfig[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int digitalTimeout[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+bool digitalLastState[] = {false, false, false, false, false, false, false, false, false, false, false, false};
 
 // Analog Pins A0-A5 Setup
 /*
   0 - Empty
+  1 - Button
 */
 
 int analogConfig[] = {0, 0, 0, 0, 0, 0};
+int analogTimeout[] = {0, 0, 0, 0, 0, 0};
+int analogLastState[] = {0, 0, 0, 0, 0, 0};
+int analogChangeMin[] = {0, 0, 0, 0, 0, 0};
+
+String serialBuffer = "";
 
 void setup() {
   Serial.begin(9600);
@@ -35,7 +43,10 @@ void setup() {
       break;
     }
     digitalConfig[i] = read;
+    // Configure digital pins (2-13)
+    configureDigitalPin(i+2);
   }
+
   Serial.println(intArrayToString(digitalConfig, 12));
   
   for (int i = 12; i < 18; i++){
@@ -51,13 +62,14 @@ void setup() {
       break;
     }
     analogConfig[i] = read;
+    // Configure analog pins (14-19/A0-A5)
+    configureAnalogPin(i+2);
   }
   Serial.println(intArrayToString(analogConfig, 6));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  handleSerialCommands();
 }
 
 String intArrayToString(int arr[], int size) {
@@ -69,4 +81,52 @@ String intArrayToString(int arr[], int size) {
     }
   }
   return output;
+}
+
+void configureDigitalPin(int pin){
+  // 1 - Button to ground
+  if (digitalConfig[pin-2] == 1){
+    pinMode(pin, INPUT_PULLUP);
+  }
+}
+
+void configureAnalogPin(int pin){
+  // 1 - Button to ground
+  if (analogConfig[pin-14] == 1){
+    pinMode(pin, INPUT_PULLUP);
+  }
+}
+
+void handleSerialCommands(){
+  while (Serial.available() > 0){
+    char serialChar = Serial.read();
+    if (serialChar == '\n'){
+      // Digital Pin Functions
+      if (serialBuffer.substring(0, 5) == "DPIN "){
+        // Set digital pin config
+        if (serialBuffer.substring(5, 7)  == "S "){
+          // Usage - "DPIN S <PIN (two digits)> <MODE (two digits)>"
+          int pin = serialBuffer.substring(7, 9).toInt();
+          int mode = serialBuffer.substring(10, 12).toInt();
+          if (pin < 2 || pin > 13){
+            Serial.println("Malfored command. Invalid pin.");
+          }
+          else{
+            digitalConfig[pin-2] = mode;
+            EEPROM.update(pin-2, mode);
+            configureDigitalPin(pin);
+            Serial.println("DIGITAL PIN " + String(pin) + " IS NOW MODE " + String(mode));
+          }
+        }
+      }
+      else{
+        Serial.println("Malformed command.");
+      }
+      Serial.println(serialBuffer);
+      serialBuffer = "";
+    }
+    else{
+      serialBuffer.concat(serialChar);
+    }
+  }
 }
