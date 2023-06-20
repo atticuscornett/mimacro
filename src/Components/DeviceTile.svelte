9c057d0a4481c7639d67ee6130fb9310e225c339
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2023-06-19 23:13:05 -0500

    Device Hover Buttons

--- src/Components/DeviceTile.svelte
+++ src/Components/DeviceTile.svelte
@@ -13,6 +13,22 @@
         <hr>
         <h2>{nickname}</h2>
         <h5 style="{(status == "outdated") ? "color: yellow;" : ""}">{mimacroVersion}{(status == "outdated") ? " (outdated)":""}</h5>
+        <div class="HoverOpt">
+            <svg xmlns="http://www.w3.org/2000/svg" class="HoverOptIcon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                <path d="M8 12l0 .01"></path>
+                <path d="M12 12l0 .01"></path>
+                <path d="M16 12l0 .01"></path>
+             </svg>
+             <div>
+                <button>Rename</button>
+                <br>
+                <button>{(status == "connected") ? "Reflash":""}{(status == "outdated") ? "Flash Update":""}</button>
+                <br>
+                <button>Remove</button>
+             </div>
+        </div>
     </div>
     {#if status == "disconnected"}
         <img id="disconnectIcon" src={"../src/Images/Icons/Error.svg"} alt="Device disconnected." title="Device disconnected.">
@@ -26,6 +42,35 @@
     .DevTileWrap {
         position: relative;
         display: inline-block;
+    }
+
+    .HoverOpt {
+        position: absolute;
+        top: 3px;
+        right: 33px;
+        display: none;
+    }
+
+    .HoverOptIcon {
+        float: right;
+    }
+
+    .DevTileWrap:hover .HoverOpt{
+        display: block;
+    }
+
+    .HoverOpt > div {
+        display: none;
+        font-size: 10px;
+    }
+
+    .HoverOpt:hover > div {
+        display: block;
+        margin-top: 30px;
+    }
+
+    .HoverOpt > div > button {
+        width: 100%;
     }
 
     .DeviceTile {
