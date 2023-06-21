562d11a5bdcb4bbb0abcb92d56b0bc139837a3d5
Roark McKibben <93952263+Djtpj@users.noreply.github.com>
2023-06-20 22:14:57 -0500

    created Action class

--- src/Macros/action.ts
+++ src/Macros/action.ts
@@ -1,9 +1,11 @@
-export interface Action {
-    // TODO create Action
+export abstract class Action {
+    abstract onRun(): void;
 }
 
 export function placeholderAction(): Action {
     return {
-
+        onRun(): void {
+            console.log("that's crazy I'm running")
+        }
     };
 }
