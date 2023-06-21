6a09a5dc5c390533790525fd345ec04b22c48332
Roark McKibben <93952263+Djtpj@users.noreply.github.com>
2023-06-21 11:02:11 -0500

    fixed bug with form validation

--- src/Macros/MacroCreator.svelte
+++ src/Macros/MacroCreator.svelte
@@ -52,6 +52,7 @@
 
     function checkCanLeave(trigger: TriggerData, action: Action, pin: Pin, macroName: string, device: ArduinoDevice, part: Part): boolean {
         if (!trigger) return false;
+        if (!trigger.name) return false;
         if (!action) return false;
         if (!pin) return false;
 
