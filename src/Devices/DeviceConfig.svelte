11fca55d7d8cd488bd1f11d5330525fb64b152ec
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2023-07-22 16:21:07 -0500

    Device Config UI Update

--- src/Devices/DeviceConfig.svelte
+++ src/Devices/DeviceConfig.svelte
@@ -36,36 +36,43 @@
     }
 </script>
 
-<h1>Configure {devices[device].nickname}</h1>
+<h2>Configure "{devices[device].nickname}"</h2>
 {#if partsList.length}
-    <div on:change={getSelections}>
-        <h3>Digital Pins</h3>
-        <div class="grid">
-            {#each devices[device].pinOut.digital as d, i}
-                <div class="config">
-                    <label for={"digital-" + i}>Pin {deviceLayouts[devices[device].mimacroType]["digital"][i]}</label>
-                    <select value={String(d)} id={"digital-" + i}>
-                        {#each partsList as part}
-                        {#if part.type != "analog"}
-                            <option value={String(part.id)}>{part.name}</option>
-                        {/if}
-                        {/each}
-                    </select>
-                </div>
-            {/each}
+    <div on:change={getSelections} style="display: inline-flex;margin: 0 20px;width:-webkit-fill-available;justify-content:space-between;">
+        <div>
+            <h3>Digital Pins</h3>
+            <div class="grid">
+                {#each devices[device].pinOut.digital as d, i}
+                    <div class="config">
+                        <label for={"digital-" + i}>Pin {deviceLayouts[devices[device].mimacroType]["digital"][i]}</label>
+                        <select value={String(d)} id={"digital-" + i}>
+                            {#each partsList as part}
+                            {#if part.type != "analog"}
+                                <option value={String(part.id)}>{part.name}</option>
+                            {/if}
+                            {/each}
+                        </select>
+                    </div>
+                {/each}
+            </div>
         </div>
-        <h3>Analog Pins</h3>
-        <div class="grid">
-            {#each devices[device].pinOut.analog as d, i}
-                <div>
-                    <label>Pin {deviceLayouts[devices[device].mimacroType]["analog"][i]}</label>
-                    <select value={String(d)} id={"analog-" + i}>
-                        {#each partsList as part}
-                            <option value={String(part.id)}>{part.name}</option>
-                        {/each}
-                    </select>
-                </div>
-            {/each}
+        <div style="height: 50vh;">
+            <img id="deviceImg" src={"../src/Images/MimacroTypes/" + devices[device].mimacroType + ".webp"} alt="Mimacro Device">
+        </div>
+        <div>
+            <h3>Analog Pins</h3>
+            <div class="grid">
+                {#each devices[device].pinOut.analog as d, i}
+                    <div>
+                        <label for={"digital-" + i}>Pin {deviceLayouts[devices[device].mimacroType]["analog"][i]}</label>
+                        <select value={String(d)} id={"analog-" + i}>
+                            {#each partsList as part}
+                                <option value={String(part.id)}>{part.name}</option>
+                            {/each}
+                        </select>
+                    </div>
+                {/each}
+            </div>
         </div>
     </div>
 {/if}
@@ -81,5 +88,12 @@
         display: grid;
         justify-content: start;
         grid-gap: 5px;
+    }
+
+    #deviceImg {
+        width: 100%;
+        max-height: 90%;
+        transform: rotate(-90deg);
+        margin-top: 20%;
     }
 </style>
