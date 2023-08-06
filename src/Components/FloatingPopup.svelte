45a774e864ab62ce7a8d74992ae324a0fd438444
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2023-08-06 15:53:38 -0500

    FloatingPopup Component

---
+++ src/Components/FloatingPopup.svelte
@@ -0,0 +1,39 @@
+<script>
+    import {onMount} from "svelte";
+
+    export let x;
+    export let y;
+
+    let placePopup = () => {
+        let modX = x;
+        let modY = y;
+        let popup = document.getElementById("floatPopup");
+        if ((Number(x) + popup.offsetWidth) > window.innerWidth){
+            modX -= popup.offsetWidth;
+        }
+        if ((Number(y) + popup.offsetHeight) > window.innerHeight){
+            modY -= popup.offsetHeight;
+        }
+        popup.style.left = modX + "px";
+        popup.style.top = modY + "px";
+    };
+    onMount(placePopup);
+    onresize = placePopup;
+</script>
+<div class="floatPopup" id="floatPopup">
+    <slot></slot>
+</div>
+<style>
+    .floatPopup {
+        background-color: white;
+        position: absolute;
+        width: max-content;
+        z-index: 9999;
+    }
+
+    @media (prefers-color-scheme: dark){
+        .floatPopup {
+            background-color: black;
+        }
+    }
+</style>
