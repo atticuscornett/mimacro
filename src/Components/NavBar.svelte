fd1201ccf220096c507e4437eadf51e6061bc98c
atticuscornett <64325242+atticuscornett@users.noreply.github.com>
2023-08-15 16:58:17 -0500

    Navbar Style Overhaul

--- src/Components/NavBar.svelte
+++ src/Components/NavBar.svelte
@@ -22,19 +22,37 @@
     ];
 </script>
 
-{#each pages as {name, pageTo}}
-    <NavButton id={pageTo} name={name} checked={name==="Devices"} bind:page={page}></NavButton>
-{/each}
-<hr>
+<div class="NavBarWrapper">
+    <div class="NavBar">
+        {#each pages as {name, pageTo}}
+            <NavButton id={pageTo} name={name} checked={name==="Devices"} bind:page={page}></NavButton>
+        {/each}
+        </div>
+    <hr>
+</div>
 
 <style>
     hr {
         color: var(--background-gray);
     }
 
+    .NavBarWrapper {
+        position: sticky;
+        padding-top: 8px;
+        top: 0;
+        background-color: white;
+    }
+
+    .NavBar {
+        padding-left: 8px;
+    }
+
     @media (prefers-color-scheme: dark) {
         hr {
             color: white;
+        }
+        .NavBarWrapper {
+            background-color: var(--background-gray);
         }
     }
 </style>
